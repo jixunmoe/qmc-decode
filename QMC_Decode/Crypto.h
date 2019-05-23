e5b7f1e392a865fd669233e211431959245e5122
Jixun Wu <jixun.moe@gmail.com>
2019-05-23 22:22:56 +0100

    Remove unused code.

--- QMC_Decode/Crypto.h
+++ QMC_Decode/Crypto.h
@@ -10,8 +10,7 @@ public:
 	~Crypto();
 
 	void transform(uint8_t* data, unsigned int offset, size_t size);
-	void transform(std::vector<uint8_t>& data, unsigned int offset, size_t size);
 
-	uint8_t encode(uint8_t c, unsigned int i);
+	uint8_t encode(unsigned int i);
 };
 
